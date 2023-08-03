79da54e92fd4a3a7703f9d47460a2fead18ec456
Dhana <dhanalakshmisundar31@gmail.com>
2023-08-03 10:43:36 +0530

    day7,16-10

--- Day-7/rest.js
+++ Day-7/rest.js
@@ -4,7 +4,7 @@ const loadCountryAPI = () =>{
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
     .then(data => displayCountries(data))
-}
+ }
 
 // displaying all countries
 const displayCountries = countries =>{
